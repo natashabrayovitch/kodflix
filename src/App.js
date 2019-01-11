d0f0332d37b782ebf97dfec46d94b376cacae258
natasa sb <natasasb@PingPing.localdomain>
2019-01-11 15:34:32 +0000

    Add 6 TV show titles #3

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={summer1} alt='' />
+        <img src={summer1} alt='summer1' />
+        <br /><br />< br/>
+        <div className='container'>
+          <div className='item'>Queen</div>
+          <div className='item' >Milke</div>
+          <div className='item' >Julia</div>
       </div>
+        <div className='container'>
+          <div className='item'>Yulia</div>
+          <div className='item' >Eva</div>
+          <div className='item' >Emma</div>
+      </div>
+
+    </div>
     );
   }
 }
