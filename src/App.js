470f9c92306da49451bae3643480462dbd4b8109
natasa sb <natasasb@natasas-MBP.home>
2019-01-11 19:29:24 +0000

    changed cover photo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import summer1 from './summer1.jpeg';
+import JinglesVA from './JinglesVA.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={summer1} alt='summer1' />
+        <img src={JinglesVA} alt='JinglesVA' />
         <br /><br />< br/>
         <div className='container'>
           <div className='item'>Queen</div>
