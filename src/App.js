c77a519d70222a1e22010f0f2f3a9ad834f70c6d
natasa sb <natasasb@natasas-MBP.home>
2019-01-11 21:06:09 +0000

    Replace TV show titles with covers #2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import JinglesVA from './JinglesVA.png';
+import summer1 from './coverImages/summer1.jpeg';
+import summer2 from './coverImages/summer2.jpeg';
+import summer3 from './coverImages/summer3.jpeg';
+import summer4 from './coverImages/summer4.jpeg';
+import summer5 from './coverImages/summer5.jpeg';
+import summer6 from './coverImages/summer6.jpeg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={JinglesVA} alt='JinglesVA' />
-        <br /><br />< br/>
+        <br /><br />< br />
         <div className='container'>
-          <div className='item'>Queen</div>
-          <div className='item' >Milke</div>
-          <div className='item' >Julia</div>
-      </div>
+          <div className='item'>
+            <img src={summer1} alt='summer1' />
+          </div>
+          <div className='item'>
+            <img src={summer2} alt='summer2' />
+          </div>
+          <div className='item'>
+            <img src={summer3} alt='summer3' />
+          </div>
+        </div>
         <div className='container'>
-          <div className='item'>Yulia</div>
-          <div className='item' >Eva</div>
-          <div className='item' >Emma</div>
-      </div>
+          <div className='item'>
+            <img src={summer4} alt='summer4' />
+          </div>
+          <div className='item' >
+            <img src={summer5} alt='summer5' />
+          </div>
+          <div className='item' >
+            <img src={summer6} alt='summer6' />
+          </div>
+        </div>
 
-    </div>
+      </div>
     );
   }
 }
