e1b31f774d42ca7f95a1bccf69dde2cdce0b085d
natasa sb <natasasb@natasas-MBP.home>
2019-01-12 21:37:20 +0000

    #9 Create a Gallery component to show the covers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import Intro from './Intro';
-import summer1 from './coverImages/summer1.jpeg';
-import summer2 from './coverImages/summer2.jpeg';
-import summer3 from './coverImages/summer3.jpeg';
-import summer4 from './coverImages/summer4.jpeg';
-import summer5 from './coverImages/summer5.jpeg';
-import summer6 from './coverImages/summer6.jpeg';
+import Gallery from './Gallery';
 
 import './App.css';
 
@@ -14,16 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <br /><br />< br />
-        <div className='container'>
-          <Intro name='Queen' logo={summer1} />
-          <Intro name='Milke' logo={summer2} />
-          <Intro name='Julia' logo={summer3} />
-        </div>
-        <div className='container'>
-          <Intro name='Yulia' logo={summer4} />
-          <Intro name='Eva' logo={summer5} />
-          <Intro name='Emma' logo={summer6} />
-        </div>
+        <Gallery />
       </div>
     );
   }
---
+++ src/Gallery.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Intro from './Intro';
+import summer1 from './coverImages/summer1.jpeg';
+import summer2 from './coverImages/summer2.jpeg';
+import summer3 from './coverImages/summer3.jpeg';
+import summer4 from './coverImages/summer4.jpeg';
+import summer5 from './coverImages/summer5.jpeg';
+import summer6 from './coverImages/summer6.jpeg';
+
+export default function Gallery() {
+    return (
+      <div>
+      <div className='container'>
+        <Intro name='Queen' logo={summer1} />
+        <Intro name='Milke' logo={summer2} />
+        <Intro name='Julia' logo={summer3} />
+    </div>
+        <div className='container'>
+          <Intro name='Yulia' logo={summer4} />
+          <Intro name='Eva' logo={summer5} />
+          <Intro name='Emma' logo={summer6} />
+        </div>
+      </div>
+    )
+  }
+  
