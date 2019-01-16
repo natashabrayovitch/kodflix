c6fe82270898304f93186210f2d0dbdb9a7e42db
natasa sb <natasasb@natasas-MacBook-Pro.local>
2019-01-16 21:01:01 +0000

    Move the covers data model to a proper front-end data source #12

---
+++ src/Gallery-get.js
@@ -0,0 +1,18 @@
+
+import summer1 from './coverImages/summer1.jpeg';
+import summer2 from './coverImages/summer2.jpeg';
+import summer3 from './coverImages/summer3.jpeg';
+import summer4 from './coverImages/summer4.jpeg';
+import summer5 from './coverImages/summer5.jpeg';
+import summer6 from './coverImages/summer6.jpeg';
+
+export default function getGallery() {
+    return [
+        {id:'Queen', name:'Queen', logo: summer1},
+        {id:'Milke', name:'Milke', logo: summer2},
+        {id:'Julia', name:'Julia', logo: summer3},
+        {id:'Yulia', name:'Yulia', logo: summer4},
+        {id:'Eva', name:'Eva', logo: summer5},
+        {id:'Emma', name:'Emma', logo: summer6}
+    ];
+}
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import Intro from './Intro';
-import summer1 from './coverImages/summer1.jpeg';
-import summer2 from './coverImages/summer2.jpeg';
-import summer3 from './coverImages/summer3.jpeg';
-import summer4 from './coverImages/summer4.jpeg';
-import summer5 from './coverImages/summer5.jpeg';
-import summer6 from './coverImages/summer6.jpeg';
+import getGallery from './Gallery-get';
 
 export default function Gallery() {
     return (
         <div>
             <div className='container'>
-                <Intro id='Queen' name='Queen' logo={summer1} />
-                <Intro id='Milke' name='Milke' logo={summer2} />
-                <Intro id='Julia' name='Julia' logo={summer3} />
-                <Intro id='Yulia' name='Yulia' logo={summer4} />
-                <Intro id='Eva' name='Eva' logo={summer5} />
-                <Intro id='Emma' name='Emma' logo={summer6} />
+            {
+             getGallery().map(intro => (
+             <Intro
+                key={intro.id}
+                id={intro.id} 
+                name={intro.name} 
+                logo={intro.logo} />
+                ))
+            }
             </div>
         </div>
-    )
+    );
 }
