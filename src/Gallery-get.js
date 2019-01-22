fc11a0ad402ff2f025429c7e52cf8e516c9de708
natasa sb <natasasb@PingPing.localdomain>
2019-01-22 14:01:21 +0000

    Changed covers

--- src/Gallery-get.js
+++ src/Gallery-get.js
@@ -1,18 +1,20 @@
 
-import AmandaAW2018 from './coverImages/AmandaAW2018.png';
-import summer2 from './coverImages/summer2.jpeg';
-import JinglesVB from './coverImages/JinglesVB.png';
 import summer4 from './coverImages/summer4.jpeg';
-import PsychoCover from './coverImages/PsychoCover.jpg';
-import summer6 from './coverImages/summer6.jpeg';
+import AmandaAW2018 from './coverImages/AmandaAW2018.png';
+import TaniaDenim18 from './coverImages/TaniaDenim18.jpg';
+import Men18 from './coverImages/Men18.jpg';
+import SessionStyling18 from './coverImages/SessionStyling18.jpg';
+import ParisStage from './coverImages/ParisStage.jpg';
+
 
 export default function getGallery() {
     return [
-        {id:'AW collection 2018', name:'AW collection 2018', logo: AmandaAW2018},
-        {id:'Milke', name:'Milke', logo: summer2},
-        {id:'JinglesVB', name:'JinglesVB', logo: JinglesVB},
         {id:'SS collection 2018', name:'SS collection 2018', logo: summer4},
-        {id:'Psycho', name:'Psycho', logo: PsychoCover},
-        {id:'Emma', name:'Emma', logo: summer6}
+        {id:'AW collection 2018', name:'AW collection 2018', logo: AmandaAW2018},
+        {id:'Denim collection 2018', name:'Denim collection 2018', logo: TaniaDenim18},
+        {id:'Men collection 2018', name:'Men collection', logo: Men18},
+        {id:'Session styling 18', name:'Session styling', logo: SessionStyling18},
+        {id:'ParisStage', name:'ParisStage', logo: ParisStage},
+        
     ];
 }
