8bd25147622343401272ea8799d025d953de6144
natasa sb <natasasb@natasas-MBP.home>
2019-01-25 01:39:08 +0000

    synopsis position changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Gallery from './Gallery';
-import Details from './Details';
+import Details from './Details/Details.js';
 import NotFound from './NotFound';
 
 import './App.css';
@@ -12,8 +12,8 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-        <h3>SS 18 collection</h3>
-        <h1>Summer Solstice</h1>
+          <h3>SS 18 collection</h3>
+          <h1>Summer Solstice</h1>
           <Switch>
             <Route exact path='/' component={Gallery} />
             <Route exact path='/not-found' component={NotFound} />
--- src/Gallery.js
+++ src/Gallery.js
@@ -6,15 +6,15 @@ export default function Gallery() {
     return (
         <div>
             <div className='container'>
-            {
-             getGallery().map(intro => (
-             <Intro
-                key={intro.id}
-                id={intro.id} 
-                name={intro.name} 
-                logo={intro.logo} />
-                ))
-            }
+                {
+                    getGallery().map(intro => (
+                        <Intro
+                            key={intro.id}
+                            id={intro.id}
+                            name={intro.name}
+                            logo={intro.logo} />
+                    ))
+                }
             </div>
         </div>
     );
