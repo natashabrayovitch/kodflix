99244a20ca25f7e1e99dc58a03d84dfb612d75f0
natasa sb <natasasb@PingPing.localdomain>
2019-01-29 14:59:19 +0000

    #19 return the list of shows in backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+const videos = require('./videos');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/videos', (req, res) => res.send(videos()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
