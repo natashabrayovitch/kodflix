cc9721bdefbb251669528c1ba7d9762571be5a46
Natasha Brayovitch <stanticdoo@gmail.com>
2019-02-11 08:46:51 +0000

    reorganized backend app.js

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3001
 const videos = require('./videos');
 
-app.get('/rest/videos', (req, res) => res.send(videos()))
-
+app.get('/rest/videos', (req, res) => {
+    res.send(videos())
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
