276da0b6370dd2efeec93da5e9bece56ae5c8812
Natasha Brayovitch <stanticdoo@gmail.com>
2019-02-13 15:18:28 +0000

    2. Production development server/ #23 / changed backend app.js

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
 const videos = require('./videos');
 
-app.get('/rest/videos', (req, res) => {
-    res.send(videos())
+app.get('/rest/videos', (req, res) => res.send(videos()));
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, '../../build')));
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
