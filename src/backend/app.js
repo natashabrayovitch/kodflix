3ddde2930a40ab0df0c18129d9058e58876ebe75
Natasha Brayovitch <stanticdoo@gmail.com>
2019-02-16 00:45:13 +0000

    package.json and backend app.js to be checked

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,11 +5,10 @@ const port = process.env.PORT || 3001;
 const getVideos = require('./videos');
 
 app.get('/rest/videos', (req, res) => res.send(getVideos()));
-
-
+console.log('xx', path.join(__dirname, '../../build'));
 app.use(express.static(path.join(__dirname, '../../build')));
-
 app.get('*', function (req, res) {
+    console.log('YYYY', req.originalUrl)
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
