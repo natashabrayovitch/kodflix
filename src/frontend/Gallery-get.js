a50919ee828d59a2166051a8a5da8935eadea4d5
Natasha Brayovitch <stanticdoo@gmail.com>
2019-02-16 01:07:04 +0000

    imported new photo and updated galerry-get. photo not show on local host

--- src/frontend/Gallery-get.js
+++ src/frontend/Gallery-get.js
@@ -5,7 +5,7 @@ import summer3 from './coverImages/summer3.jpeg';
 import summer4 from './coverImages/summer4.jpeg';
 import summer5 from './coverImages/summer5.jpeg';
 import summer6 from './coverImages/summer6.jpeg';
-
+import logo from './coverImages/JinglesVB.png';
 export default function getGallery() {
     return [
         { id: 'queen', name: 'Queen Bee', logo: summer1, details: 'The Queen of the collection has got amazing bone structure, alongside determination and an agenda! Step away define her head shape and work with the facial characteristics - working hair with your hands to create this beautiful shape.', credits: "Hair by: Andrew Jose and Evelina Lundgren, Makeup by: Julia Wilson, Fashion Styling: Rhona Emma, Photography: Alex Barron-Hough, Products: TIGI Copyright colour and Catwalk, Text by: Salon News Asia professional hair magazine" },
@@ -14,5 +14,6 @@ export default function getGallery() {
         { id: 'yulia', name: 'The 2018s Freckles', logo: summer4, details: "Sharp shape with different length accenting movement throughout the  hair. A Salt Spray finish from TIGI gets Julia prepped for the beach party.", credits: "Hair by: Andrew Jose and Evelina Lundgren, Makeup by: Julia Wilson, Fashion Styling: Rhona Emma, Photography: Alex Barron-Hough, Products: TIGI Copyright colour and Catwalk, Text by: Salon News Asia professional hair magazine" },
         { id: 'eva', name: 'Cathy Come Home', logo: summer5, details: "A touch of sunshine - peach coloured full fringe to accentuate the definition of Eva's sexy eyes. Layered waves finish.", credits: "Hair by: Andrew Jose and Evelina Lundgren, Makeup by: Julia Wilson, Fashion Styling: Rhona Emma, Photography: Alex Barron-Hough, Products: TIGI Copyright colour and Catwalk, Text by: Salon News Asia professional hair magazine" },
         { id: 'emma', name: 'Heaven On Earth', logo: summer6, details: "The sun shining on the red hair reminds of heaven on earth. Soft graduation on the sides and through the back blending into one length on top. Feminine curly finish with detailed separation in the fringe.", credits: "Hair by: Andrew Jose and Evelina Lundgren, Makeup by: Julia Wilson, Fashion Styling: Rhona Emma, Photography: Alex Barron-Hough, Products: TIGI Copyright colour and Catwalk, Text by: Salon News Asia professional hair magazine" },
+        { id: 'logo', name: 'Jingles Hair Education', logo: logo, details: "London hairdressing Education for qualified haidressers Jingles style,independent training focus on trends creativity."},
  ];
 }
