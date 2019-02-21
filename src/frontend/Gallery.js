6c043b4cf96af7b14787b1712f57f9e59c9dbd2a
Natasha Brayovitch <stanticdoo@gmail.com>
2019-02-21 21:26:47 +0000

    created stateful component in frontend gallery.js (24)

--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Intro from './Intro';
-import getGallery from './Gallery-get';
+//import getGallery from './Gallery-get';
 
-export default function Gallery() {
-    return (
-        <div>
-            <div className='container'>
-                {
-                    getGallery().map(intro => (
-                        <Intro
-                            key={intro.id}
-                            id={intro.id}
-                            name={intro.name}
-                            logo={intro.logo} />
-                    ))
-                }
+
+export default class Gallery extends Component {
+    constructor() {
+        super()
+        this.state = {
+            movies: []
+        }
+    }
+    componentDidMount() {
+        fetch('/rest/intro')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (movies) {
+                this.setState({ movies: movies })
+            });
+    }
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    {
+                        this.state.movies.map(movie => (
+                            <Intro id={movie.id} name={movie.name} />
+                        ))
+                    }
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
