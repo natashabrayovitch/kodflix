8e6794941ceb8461d56648d78a0ee2966f961186
Natasha Brayovitch <stanticdoo@gmail.com>
2019-03-01 21:27:21 +0000

    #28 Connect the kodflix app with our database!

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const db = require('./db');
 const port = process.env.PORT || 3001;
-const getVideos = require('./videos');
 
-app.get('/rest/videos', (req, res) => res.send(getVideos()));
-console.log('xx', path.join(__dirname, '../../build'));
-app.use(express.static(path.join(__dirname, '../../build')));
-app.get('*', function (req, res) {
-    console.log('YYYY', req.originalUrl)
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-});
+db.connect().then(dbo => {
 
-app.listen(port, () => console.log(`Kodflix app listening on port ${port}!`))
+    app.get('/rest/shows', (req, res) => {
+        dbo.collection('shows').find({}).toArray((err, results) => {
+            if (err) throw err;
+            res.send(results);
+        });
+    });
+
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../../build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+    });
+
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+}); 
